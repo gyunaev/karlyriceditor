9ee29945ec3555720ae8746aa165f938c37ed4f4
gyunaev <gyunaev@b0e1fde3-d487-4427-aff4-b379651cbde3>
2009-11-28 04:15:20 +0000

    Added Phonon metatag hack.

--- src/phononhelper.cpp
+++ src/phononhelper.cpp
@@ -36,10 +36,41 @@ QString PhononHelper::getTag( Phonon::MediaObject * phonon, Phonon::MetaData tag
 
 	for ( int i = 0; i < data.size(); i++ )
 	{
+		// Phonon seems to return non-English tags in UTF8 as QString,
+		// i.e. done by QString( tag ), not QString::fromUtf8( tag ).
+		// Therefore heuristics tries to detect whether it is UTF8, and converts it.
+		QString linedata = data[i];
+		QByteArray encodedstring;
+		bool found_128_255 = false;
+
+		for ( int k = 0; k < linedata.size(); k++ )
+		{
+			unsigned short unichar = linedata[k].unicode();
+
+			if ( unichar > 256 )
+			{
+				encodedstring.clear();
+				break;
+			}
+			else
+			{
+				encodedstring.push_back( (char) unichar );
+
+				if ( unichar > 127 )
+					found_128_255 = true;
+			}
+		}
+
+		if ( !encodedstring.isEmpty() && found_128_255 && !QString::fromUtf8( encodedstring ).isEmpty() )
+		{
+			qDebug("Phonon hack: converting double-converted tag back to UTF8");
+			linedata = QString::fromUtf8( encodedstring );
+		}
+
 		if ( !text.isEmpty() )
 			text += ", ";
 
-		text += data[i];
+		text += linedata;
 	}
 
 	return text;
