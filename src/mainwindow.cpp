0a7f9501861ec0a97069143543d6c92d066b2b16
gyunaev <gyunaev@b0e1fde3-d487-4427-aff4-b379651cbde3>
2013-05-29 00:47:01 +0000

    Updated copyright

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -617,8 +617,8 @@ void MainWindow::act_helpAbout()
 	ui_about.setupUi( &dlg );
 
 	ui_about.labelAbout->setText( tr("<b>Karaoke Lyrics Editor version %1.%2</b><br><br>"
-			"Copyright (C) George Yunaev 2009-2011, <a href=\"mailto:support@karlyriceditor.com\">support@karlyriceditor.com</a><br><br>"
-			"Web site: <a href=\"http://www.karlyriceditor.com\">www.karlyriceditor.com</a><br><br>"
+			"Copyright (C) George Yunaev 2009-2013, <a href=\"mailto:support@ulduzsoft.com\">support@ulduzsoft.com</a><br><br>"
+			"Web site: <a href=\"http://www.ulduzsoft.com\">www.ulduzsoft.com/karlyriceditor</a><br><br>"
 			"This program is licensed under terms of GNU General Public License "
 			"version 3; see LICENSE file for details.") .arg(APP_VERSION_MAJOR) .arg(APP_VERSION_MINOR) );
 
