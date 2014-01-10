9c186e6c715265b1913736ffc80ed2823c58dbb9
gyunaev <gyunaev@b0e1fde3-d487-4427-aff4-b379651cbde3>
2014-01-10 01:47:27 +0000

    Switched to system headers, removed fifo

--- src/ffmpeg_headers.h
+++ src/ffmpeg_headers.h
@@ -29,12 +29,11 @@
 extern "C"
 {
 
-#include "libavcodec/avcodec.h"
-#include "libavformat/avformat.h"
-#include "libswscale/swscale.h"
-#include "libavresample/avresample.h"
-#include "libavutil/opt.h"
-#include "libavutil/fifo.h"
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavresample/avresample.h>
+#include <libavutil/opt.h>
 
 };
 
